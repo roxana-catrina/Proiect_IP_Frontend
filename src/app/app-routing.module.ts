ac0eb44a8754448d7e8fa430bc482e31fe083e49
Ionela <ionellaccl@gmail.com>
2025-03-26 19:23:42 +0200

    am facut interfata de dashboard pentru pacienti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,19 @@ import { InregistrareComponent } from './components/inregistrare/inregistrare.co
 import { PatientLoginComponent } from './components/patient-login/patient-login.component';
 import { DoctorLoginComponent } from './components/doctor-login/doctor-login.component';
 import { DashboardDoctorComponent } from './components/dashboard-doctor/dashboard-doctor.component';
+import { DashboardPatientComponent } from './components/dashboard-patient/dashboard-patient.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'patient', component: PatientComponent },
+  { path: 'patient', component: DashboardPatientComponent },
   { path: 'inregistrare', component: InregistrareComponent },
   { path: 'patient-login', component: PatientLoginComponent },
   { path: 'doctor-login', component: DoctorLoginComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   //{ path: '**', redirectTo: '/home' },
-  {path: 'doctor', component: DashboardDoctorComponent}
+  {path: 'doctor', component: DashboardDoctorComponent},
+  //{path: 'patient', component: DashboardPatientComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/dashboard-patient/dashboard-patient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-patient',
+  templateUrl: './dashboard-patient.component.html',
+  styleUrls: ['./dashboard-patient.component.css']
+})
+export class DashboardPatientComponent {
+
+}
