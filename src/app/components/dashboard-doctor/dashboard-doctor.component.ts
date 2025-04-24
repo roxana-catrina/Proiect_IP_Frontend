3c4f12067824c025f0a0402f8af762e3e8d3cd0d
roxana-catrina <roxanacatrina22@gmail.com>
2025-04-24 21:26:41 +0300

    doar afiseaza pacientii doctorului

--- src/app/components/dashboard-doctor/dashboard-doctor.component.ts
+++ src/app/components/dashboard-doctor/dashboard-doctor.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Patient } from '../../models/patient';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { StorageService } from '../../services/storage/storage.service';
+import { PatientService } from '../../services/patient/patient.service';
+import { DoctorService } from '../../services/doctor/doctor.service';
+import { AlertService } from '../../services/alert/alert.service';
+import { Doctor } from '../../models/doctor';
 @Component({
   selector: 'app-dashboard-doctor',
    templateUrl: './dashboard-doctor.component.html',
@@ -11,10 +17,35 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, RouterModule, FormsModule]
  
 })
-export class DashboardDoctorComponent {
+export class DashboardDoctorComponent implements OnInit {
   searchTerm: string = '';
   allPatients: Patient[] = []; // Store all patients
   patients: Patient[] = []; // Filtered patients
+  doctor: Doctor | null = null;
+  constructor(private authService: AuthService, private patientService: PatientService,
+    private doctorService: DoctorService, private storageService: StorageService,
+    private alertService: AlertService,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    const doctor = StorageService.getDoctor();
+    if (doctor && doctor.email) {
+      this.loadDoctorData(doctor.email);
+    }
+  }
+
+  private loadDoctorData(email: string) {
+    this.authService.getDoctorAllPatients(email).subscribe({
+      next:  (patients: Patient[]) => {
+        this.allPatients = patients;
+        this.patients = [...patients]; // Copy for filtering
+        console.log('Loaded patients:', this.patients);
+      },
+      error: (error: any) => {
+        console.error('Error loading patient data:', error);
+      }
+    });
+  }
 
   searchPatients() {
     if (!this.searchTerm) {
