26f9477f5c54e93f709eefe2c289f00bf32cbf24
roxana-catrina <roxanacatrina22@gmail.com>
2025-05-06 14:05:50 +0300

    mici chestiute

--- src/app/components/dashboard-doctor/dashboard-doctor.component.ts
+++ src/app/components/dashboard-doctor/dashboard-doctor.component.ts
@@ -37,6 +37,23 @@ export class DashboardDoctorComponent implements OnInit {
     }
   }
 
+  calculateAge(birthDate: Date): number {
+    if (!birthDate) {
+      return 0;
+    }
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDiff = today.getMonth() - birth.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+      age--;
+    }
+    
+    return age;
+  }
+
+
   private loadDoctorData(email: string) {
     // First, load doctor details
     this.authService.getDoctorByEmail(email).subscribe({
